6ecca382399b4851c33a4e089aec92f8b80c1bc1
Maxwell Henderson <mxwhenderson@gmail.com>
2022-12-04 09:19:24 -0800

    Add firefox profile launching

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::process::{Command, Stdio};
+
+pub fn open_profile(profile_name: String) {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/C")
+            .arg(format!("firefox -P {profile_name}"))
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Coudln't run firefox")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("firefox -P {profile_name}"))
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Couldn't run firefox")
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use ff_profile_switcher::open_profile;
+
+fn main() {
+    open_profile(String::from("temp"));
+}
