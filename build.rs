c7564e41410f9c876fbcd28a3694d98df0fa5c2a
Maxwell Henderson <mxwhenderson@gmail.com>
2022-12-04 19:09:56 -0800

    Stop build script from erroring
    Build script would error when ran because the directory it wanted to
    make was already made, just made it check if it exists before it
    tries to create it.

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use directories::ProjectDirs;
 
 fn main() {
-    let ffdir = ProjectDirs::from("io.github.maxstrid", "FFPViewier", "FF Profile Viewier").unwrap();
+    let ffdir =
+        ProjectDirs::from("io.github.maxstrid", "FFPViewier", "FF Profile Viewier").unwrap();
 
     let dir = ffdir.data_dir().to_str().unwrap();
 
-    std::fs::create_dir(dir).unwrap();
+    if !std::path::Path::new(dir).exists() {
+        std::fs::create_dir(dir).unwrap();
+    }
 
     println!("cargo:rerun-if-changed={dir}");
 }
